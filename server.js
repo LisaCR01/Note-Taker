0169bd20822fdc9af3976d2481f9273cb0547ce0
LisaCR01 <lisagunn01@gmail.com>
2022-03-15 12:32:40 +0000

    added definition for api got rid of unnecessary code

--- server.js
+++ server.js
@@ -3,41 +3,23 @@ const uuid = require('uuid');
 const path = require('path');
 const fs = require('fs');
 const dbData=require('./db/db.json')
+const api = require('./routes/index.js')
 //sets the port to work on heroku
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-//sets default set up for express
+// setting up middleware using express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use('/api', api)
 app.use(express.static('public'));
 //computer is directed to the index.html 
- app.get('/', (req, res) =>
+app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html')));
 
 //computer is directed through /notes to the notes.html 
-  app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html')));
-
-//computer is routed to dbData if /api/notes selected. 
-app.get('/api/notes', (req, res) => {
-      res.status(200).json(dbData);
-    });
-
-app.post("/api/notes", (req, res) => {
-    let newNote = req.body;
-    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    //create new property called id based on length and assign it to each json object
-  newNote.id = uuid.v4();
-  //push updated note to the data containing notes history in db.json
-  noteList.push(newNote);
-
-  //write the updated data to db.json
-  fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
-  res.json(noteList);
-window.location.reload();
-});
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))); 
 
 //computer selects a note to delete based on the id. 
 app.delete("/api/notes/:id", (req, res) => {
